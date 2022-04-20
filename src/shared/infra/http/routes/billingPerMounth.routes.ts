c7dc3468c4cfaa48be60b9815850ff7af1904921
Junior Sousa <jrsousadev@gmail.com>
2022-04-20 16:41:20 -0300

    fix biiling

--- src/shared/infra/http/routes/billingPerMounth.routes.ts
+++ src/shared/infra/http/routes/billingPerMounth.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { CreateBillingPerMounthController } from "../../../../modules/BillingPerMounth/UseCases/CreateBillingPerMounth/CreateBillingPerMounthController";
+import { GetBillingPerMounthController } from "../../../../modules/BillingPerMounth/UseCases/GetBillingPerMounth/GetBillingPerMounthController";
 
 const billingPerMounthRoutes = Router();
 
@@ -7,5 +8,8 @@ const billingPerMounthRoutes = Router();
 const createBillingPerMounthController = new CreateBillingPerMounthController();
 billingPerMounthRoutes.post('/create', createBillingPerMounthController.handle);
 
+// Get
+const getBillingPerMounthController = new GetBillingPerMounthController();
+billingPerMounthRoutes.get('/:userId', getBillingPerMounthController.handle);
 
 export default billingPerMounthRoutes;
